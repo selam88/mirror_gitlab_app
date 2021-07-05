4687fb94373b02421e292959dde974e944167da5
Selim Amrari <selim.amrari.pro@protonmail.ch>
2021-07-05 21:58:19 +0000

    test flake8

---
+++ src/apps/test_import.py
@@ -0,0 +1,10 @@
+import argparse, os
+from ..data_utils import load_training_data
+import train_utils as t_u
+import numpy as np
+import subprocess
+
+
+# main routine
+if __name__ == "__main__":
+    print("import successfull")
